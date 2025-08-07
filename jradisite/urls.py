071656ffed1c26d1625c89b43688249360e855f1
Maissene Jradi <maissenejradi1@gmail.com>
2025-08-07 12:16:06 +0100

    review and rating sytem

--- jradisite/urls.py
+++ jradisite/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('accounts/' , include('accounts.urls')),
     #orders
     path('orders/' , include('orders.urls')) ,
+    path('avatar/', include('avatar.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
