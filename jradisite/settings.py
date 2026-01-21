ff77d9619b51bb9b5d4bf0ba06c2851e7140bdc3
Maissene Jradi <maissenejradi1@gmail.com>
2026-01-21 17:53:04 +0100

    Fix EMAIL_BACKEND default to prevent EB crash

--- jradisite/settings.py
+++ jradisite/settings.py
@@ -146,7 +146,10 @@ MESSAGE_TAGS = {
 
 
 #SMTP configuration
-EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_BACKEND = config(
+    'EMAIL_BACKEND',
+    default='django.core.mail.backends.console.EmailBackend'
+)
 EMAIL_HOST = config('EMAIL_HOST', default='localhost')
 EMAIL_PORT = config('EMAIL_PORT' , default=25, cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
