ee871391c4cb5415c97612a2fb65b4ec12e3badd
Maissene Jradi <maissenejradi1@gmail.com>
2026-01-21 18:11:24 +0100

    Fix EMAIL_BACKEND default for Elastic Beanstalk

--- jradisite/settings.py
+++ jradisite/settings.py
@@ -145,14 +145,15 @@ MESSAGE_TAGS = {
 }
 
 
-#SMTP configuration
 EMAIL_BACKEND = config(
     'EMAIL_BACKEND',
-    default='django.core.mail.backends.console.EmailBackend'
+    default='django.core.mail.backends.smtp.EmailBackend'
 )
+
 EMAIL_HOST = config('EMAIL_HOST', default='localhost')
-EMAIL_PORT = config('EMAIL_PORT' , default=25, cast=int)
+EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
