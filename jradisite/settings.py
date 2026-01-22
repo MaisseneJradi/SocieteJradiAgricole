8bc684c108c5cedf8fe3e6738e34fa68fdcf37e7
Maissene Jradi <maissenejradi1@gmail.com>
2026-01-22 21:58:11 +0100

    Fix S3 ACL configuration

--- jradisite/settings.py
+++ jradisite/settings.py
@@ -184,7 +184,7 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
 AWS_LOCATION = 'media'
 
 # Configuration des fichiers média
