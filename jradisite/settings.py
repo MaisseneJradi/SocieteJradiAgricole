94ae5d749c0c7cf99ee96fa846f60e776299c140
Maissene Jradi <maissenejradi1@gmail.com>
2026-01-22 14:30:54 +0100

    Fix db

--- jradisite/settings.py
+++ jradisite/settings.py
@@ -101,27 +101,24 @@ AUTH_USER_MODEL = 'accounts.Account'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 #Configuration de la base de données
-if 'RDS_HOSTNAME' in os.environ:
-    # Production avec PostgreSQL RDS
+if os.environ.get('RDS_HOSTNAME'):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+            'NAME': os.environ.get('RDS_DB_NAME'),
+            'USER': os.environ.get('RDS_USERNAME'),
+            'PASSWORD': os.environ.get('RDS_PASSWORD'),
+            'HOST': os.environ.get('RDS_HOSTNAME'),
+            'PORT': os.environ.get('RDS_PORT', '5432'),
         }
     }
 else:
-    # Développement local avec SQLite
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
