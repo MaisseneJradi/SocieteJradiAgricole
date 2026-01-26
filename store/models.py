1b7f1a25db6838dad5f3c0020eb2f79ee404f7b5
Maissene Jradi <maissenejradi1@gmail.com>
2026-01-26 11:36:00 +0100

    last last committt

--- store/models.py
+++ store/models.py
@@ -45,7 +45,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(max_length=5000, blank=True)
-    price = models.TextField(max_length=1000)
+    price = models.FloatField()
     images = models.ImageField(upload_to='photos/products')
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
